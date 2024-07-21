556128a404806eda6962e049192b022b567a1610
Angelo Netho <angelonetho@gmail.com>
2024-07-21 03:36:55 -0300

    Respostas com anexos

--- src/infra/http/controllers/edit-answer.controller.ts
+++ src/infra/http/controllers/edit-answer.controller.ts
@@ -14,6 +14,7 @@ import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-ans
 
 const editAnswerBodySchema = z.object({
   content: z.string(),
+  attachments: z.array(z.string().uuid()).default([]),
 })
 
 const bodyValidationPipe = new ZodValidationPipe(editAnswerBodySchema)
@@ -31,7 +32,7 @@ export class EditAnswerController {
     @CurrentUser() user: UserPayload,
     @Param('id') answerId: string,
   ) {
-    const { content } = body
+    const { content, attachments } = body
 
     const userId = user.sub
 
@@ -39,7 +40,7 @@ export class EditAnswerController {
       answerId,
       content,
       authorId: userId,
-      attachmentsIds: [],
+      attachmentsIds: attachments,
     })
 
     if (result.isLeft()) {
