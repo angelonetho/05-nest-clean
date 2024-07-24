c1d80f782f354e4c414015f91300e23865eb48a8
Angelo Netho <angelonetho@gmail.com>
2024-07-24 15:38:09 -0300

    Implementando Cache com Redis

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common'
 import { EnvModule } from '../env/env.module'
-import { EnvService } from '../env/env.service'
 import { CacheRepository } from './cache-repository'
 import { RedisCacheRepository } from './redis/redis-cache-repository'
 import { RedisService } from './redis/redis.service'
